a278f90f6997e16f68daf9d3cb5af01887e9b83e
phoenix-1-2 <phoenix.redstone.04@gmail.com>
2021-07-09 18:51:11 +0530

    Auth Added

--- medical_store/views.py
+++ medical_store/views.py
@@ -12,15 +12,14 @@ import jwt
 
 ENCRIPTION_KEY = os.environ.get("ENCRIPTION_KEY")
 
-
 @api_view(["POST"])
 def signup(request):
-    shopkeeper_name = request.body.get("shopkeeper_name")
-    email = request.body.get("email")
-    password = request.body.get("password")
-    confirmPassword = request.body.get("confirmPassword")
-    shopkeeper_contact_number = request.body.get("shopkeeper_contact_number")
-    shop_address = request.body.get("shop_address")
+    shopkeeper_name = request.data.get("shopkeeper_name")
+    email = request.data.get("email")
+    password = request.data.get("password")
+    confirmPassword = request.data.get("confirmPassword")
+    shopkeeper_contact_number = request.data.get("shopkeeper_contact_number")
+    shop_address = request.data.get("shop_address")
     is_verfied = False
     company_tie_ups = []
     medicines = []
@@ -51,44 +50,44 @@ def signup(request):
     # SEND EMAIL TO USER EMAIL_ID
 
 
-# @api_view(["GET"])
-# def verify(request, shopkeeper_id, token):
+@api_view(["GET"])
+def verify(request, shopkeeper_id, token):
 
-#     try:
-#         profile = Shopkeeper.objects.get(
-#             {
-#                 "$and": {
-#                     "shopkeeper_id": shopkeeper_id,
-#                     "token": token,
-#                     "verification_token_expiry_time": {"$gt": datetime.datetime.now()},
-#                 }
-#             }
-#         )
-#         profile.is_verfied = True
-#         profile.verification_token_expiry_time = None
-#         profile.verification_token = None
-#         profile.save()
-#     except Shopkeeper.DoesNotExist:
-#         return Response({"message": "Time Invalid"}, 403)
+    try:
+        profile = Shopkeeper.objects.get(
+            {
+                "$and": {
+                    "shopkeeper_id": shopkeeper_id,
+                    "token": token,
+                    "verification_token_expiry_time": {"$gt": datetime.datetime.now()},
+                }
+            }
+        )
+        profile.is_verfied = True
+        profile.verification_token_expiry_time = None
+        profile.verification_token = None
+        profile.save()
+    except Shopkeeper.DoesNotExist:
+        return Response({"message": "Time Invalid"}, 403)
 
 
-# @api_view("POST")
-# def login(request):
-#     email = request.body.get("email")
-#     password = request.body.get("password")
+@api_view(["POST"])
+def login(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
 
-#     try:
-#         profile = Shopkeeper.objects.get(
-#             {
-#                 "email": email,
-#             }
-#         )
-#         decoded_password = cryptocode.decrypt(profile.password, ENCRIPTION_KEY)
-#         if decoded_password != password:
-#             return Response({"message": "Invalid Password,Shopkeeper Not Found"}, 404)
-#         encoded_jwt = jwt.encode(
-#             {"shopkeeper_id": profile.shopkeeper_id}, ENCRIPTION_KEY, algorithm="HS256"
-#         )
-#         return Response({"Authorization": "Bearer " + encoded_jwt}, 200)
-#     except Shopkeeper.DoesNotExist:
-#         return Response({"message": "Invalid Credentials,Shopkeeper Not Found"}, 404)
+    try:
+        profile = Shopkeeper.objects.get(
+            {
+                "email": email,
+            }
+        )
+        decoded_password = cryptocode.decrypt(profile.password, ENCRIPTION_KEY)
+        if decoded_password != password:
+            return Response({"message": "Invalid Password,Shopkeeper Not Found"}, 404)
+        encoded_jwt = jwt.encode(
+            {"shopkeeper_id": profile.shopkeeper_id}, ENCRIPTION_KEY, algorithm="HS256"
+        )
+        return Response({"Authorization": "Bearer " + encoded_jwt}, 200)
+    except Shopkeeper.DoesNotExist:
+        return Response({"message": "Invalid Credentials,Shopkeeper Not Found"}, 404)
