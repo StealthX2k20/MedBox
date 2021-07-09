6b11765c298f9a0b92285e12929b63bee697cbbd
phoenix-1-2 <phoenix.redstone.04@gmail.com>
2021-07-09 18:16:26 +0530

    [feature/signup] : Signup code added

--- medical_store/views.py
+++ medical_store/views.py
@@ -51,44 +51,44 @@ def signup(request):
     # SEND EMAIL TO USER EMAIL_ID
 
 
-@api_view(["GET"])
-def verify(request, shopkeeper_id, token):
+# @api_view(["GET"])
+# def verify(request, shopkeeper_id, token):
 
-    try:
-        profile = Shopkeeper.objects.get(
-            {
-                "$and": {
-                    "shopkeeper_id": shopkeeper_id,
-                    "token": token,
-                    "verification_token_expiry_time": {"$gt": datetime.datetime.now()},
-                }
-            }
-        )
-        profile.is_verfied = True
-        profile.verification_token_expiry_time = None
-        profile.verification_token = None
-        profile.save()
-    except Shopkeeper.DoesNotExist:
-        return Response({"message": "Time Invalid"}, 403)
+#     try:
+#         profile = Shopkeeper.objects.get(
+#             {
+#                 "$and": {
+#                     "shopkeeper_id": shopkeeper_id,
+#                     "token": token,
+#                     "verification_token_expiry_time": {"$gt": datetime.datetime.now()},
+#                 }
+#             }
+#         )
+#         profile.is_verfied = True
+#         profile.verification_token_expiry_time = None
+#         profile.verification_token = None
+#         profile.save()
+#     except Shopkeeper.DoesNotExist:
+#         return Response({"message": "Time Invalid"}, 403)
 
 
-@api_view("POST")
-def login(request):
-    email = request.body.get("email")
-    password = request.body.get("password")
+# @api_view("POST")
+# def login(request):
+#     email = request.body.get("email")
+#     password = request.body.get("password")
 
-    try:
-        profile = Shopkeeper.objects.get(
-            {
-                "email": email,
-            }
-        )
-        decoded_password = cryptocode.decrypt(profile.password, ENCRIPTION_KEY)
-        if decoded_password != password:
-            return Response({"message": "Invalid Password,Shopkeeper Not Found"}, 404)
-        encoded_jwt = jwt.encode(
-            {"shopkeeper_id": profile.shopkeeper_id}, ENCRIPTION_KEY, algorithm="HS256"
-        )
-        return Response({"Authorization": "Bearer " + encoded_jwt}, 200)
-    except Shopkeeper.DoesNotExist:
-        return Response({"message": "Invalid Credentials,Shopkeeper Not Found"}, 404)
+#     try:
+#         profile = Shopkeeper.objects.get(
+#             {
+#                 "email": email,
+#             }
+#         )
+#         decoded_password = cryptocode.decrypt(profile.password, ENCRIPTION_KEY)
+#         if decoded_password != password:
+#             return Response({"message": "Invalid Password,Shopkeeper Not Found"}, 404)
+#         encoded_jwt = jwt.encode(
+#             {"shopkeeper_id": profile.shopkeeper_id}, ENCRIPTION_KEY, algorithm="HS256"
+#         )
+#         return Response({"Authorization": "Bearer " + encoded_jwt}, 200)
+#     except Shopkeeper.DoesNotExist:
+#         return Response({"message": "Invalid Credentials,Shopkeeper Not Found"}, 404)
